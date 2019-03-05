fb7e8ffbc40c7099dab5adcaf8f3b896ab4d8032
jing.dong <dj3386083@gmail.com>
2019-03-06 09:45:39 +1100

    auto email

---
+++ bodgermail/app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+  default from:"mayor@badgertown.com"
+
+  def welcome(user)
+    @user = user #@user will be whatever user we pass in to the 'welcome' method
+    mail( :to => @user.email, :subject => 'Welcome to ')
+  end
+end
